3d537377bb5d6d657758ccf4820569d806e04e5c
rahmat <fayeq.rahmat@gmail.com>
2025-09-16 06:40:45 +0430

    added website url

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,7 +22,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                     </svg>
-                    <a href="$" target="_blank"
+                    <a href="https://rahmat.vercel.app" target="_blank"
                         class="text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-300 transition-colors">
                         Our site
                     </a>
